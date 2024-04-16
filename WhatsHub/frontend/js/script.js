81ce4944946ca4279e69ffe974d9e40d94984ffc
andreanjunior <165954516+andreanjunior@users.noreply.github.com>
2024-04-16 15:58:08 -0300

    Update script.js

--- WhatsHub/frontend/js/script.js
+++ WhatsHub/frontend/js/script.js
@@ -77,7 +77,7 @@ const handleLogin = (event) => {
   login.style.display = "none";
   chat.style.display = "flex";
 
-  websocket = new WebSocket("ws://localhost:8080");
+  websocket = new WebSocket("wss://chatbee.onrender.com");
   websocket.onmessage = processMessage;
 };
 const sendMessage = (event) => {
